0252497eb32bbcc0278ff1b28242c340683a0152
sd7822735 <sd7822735@outlook.com>
2022-08-08 16:05:11 +0800

    SignalR姓名微調

--- Medical/wwwroot/js/chat.js
+++ Medical/wwwroot/js/chat.js
@@ -8,19 +8,38 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (sendUser, message) {
     let user = $("#inpUser").val();
+    let userName = $("#inpUser2").val();
     var MSN = document.querySelector("#MSN");
     var time = moment(Date.now()).format("LTS")
     var role = "訪客";
     var role1 = "客服";
+    var userName1 = "訪客";
+    var userName2 = "客服";
+    if (userName == "") {
+        userName1 = "訪客";
+    }
+    if (user == 1) {
+        userName1 = userName;
+        userName2 = "客服";
+    }
     if (user == 2 || user == 3) {
         role = "客服";
         role1 = "訪客";
+        userName1 = "客服";
+        if (userName == "") {
+            userName2 = "訪客";
+        }
+        //這邊是寫死的，之後要優化可以優化
+        else {
+            userName2 = "陳冠名";
+        }
     }
+   
     if (user == sendUser) {
         
         $("#MSN").append(`<div class="direct-chat-msg text-md-end">
                                 <div class="direct-chat-infos clearfix">
-                                    <span class="direct-chat-name float-left" id="name_player1">《${role}》</span>
+                                    <span class="direct-chat-name float-left" id="name_player1">《${userName1}》</span>
                                     <span class="direct-chat-timestamp float-right" id="time_player1">(${time})</span>
                                 </div>
                                 <span class="text-bold" style="font-size:25px;background-color:#ACD6FF;border-radius:10px;border:2px solid #F0F0F0">   ${message}  </span>
@@ -33,7 +52,7 @@ connection.on("ReceiveMessage", function (sendUser, message) {
     else{
         $("#MSN").append(`<div class="direct-chat-msg">
                         <div class="direct-chat-infos clearfix">
-                            <span class="direct-chat-name float-left" id="name_player1">《${role1}》</span>
+                            <span class="direct-chat-name float-left" id="name_player1">《${userName2}》</span>
                             <span class="direct-chat-timestamp float-right" id="time_player1">(${time})</span>
                         </div>
                         <img class="direct-chat-img" src="../img/${role1}.jpg" alt="Message User Image" style="border-radius:50%">
@@ -44,25 +63,6 @@ connection.on("ReceiveMessage", function (sendUser, message) {
                     </div>`)
     }
 });
-
-
-    //可新增
-    //else {
-    //    $("#MSN").append(`<div class="direct-chat-msg right text-md-start">
-    //                    <div class="direct-chat-infos clearfix">
-    //                        <span class="direct-chat-name float-left" id="name_player1">${user}</span>
-    //                        <span class="direct-chat-timestamp float-right" id="time_player1">${time}</span>
-    //                    </div>
-    //                    <img class="direct-chat-img" src="../img/訪客.jpg" alt="Message User Image" style="border-radius:50%">
-    //                <div class="direct-chat-text">
-    //                    <ul id="message_player1">
-    //                       <li>${message}</li>
-    //                    </ul>
-    //                </div>
-    //                </div>`)
-    //}
-/*});*/
-
 
 
 connection.start().then(function () {
